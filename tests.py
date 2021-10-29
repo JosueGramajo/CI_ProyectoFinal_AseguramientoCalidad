3869dffcb74ea23c2d23847122a49340c81c1f5a
Josue Gramajo <jgramajo@digitalgeko.com>
2021-10-28 18:15:28 -0600

    Cambios para el ambiente DEV

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from hello import app
 with app.test_client() as c:
 	response = c.get('/')
-	assert response.data == b'Hola Mundo!, Este es el ambiente de QA'
+	assert response.data == b'Hola Mundo!, Este es el ambiente de DEV'
 	assert response.status_code == 200
